4beba5d52c567a275d2618c93d6badd64a4d8d5f
Gabriel Antognoli <gabriel_antognoli@hotmail.com>
2025-03-01 12:53:17 -0300

    Projeto finalizado

--- lib/components/Cronometro.dart
+++ lib/components/Cronometro.dart
@@ -10,29 +10,31 @@ class Cronometro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final store = Provider.of<PomodoroStore>(context);
-    return Container(
-      color: Colors.red,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Hora de Trabalhar',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 40,
+    return Observer(
+      builder: (context) => Container(
+        color: store.estaTrabalhando() ? Colors.red : Colors.green,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              store.estaTrabalhando()
+                  ? 'Hora de Trabalhar'
+                  : 'Hora de Descansar',
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+              ),
             ),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            '${store.minutos.toString().padLeft(2, '0')}:${store.segundos.toString().padLeft(2, '0')}',
-            style: const TextStyle(
-              fontSize: 120,
-              color: Colors.white,
+            const SizedBox(height: 20),
+            Text(
+              '${store.minutos.toString().padLeft(2, '0')}:${store.segundos.toString().padLeft(2, '0')}',
+              style: const TextStyle(
+                fontSize: 120,
+                color: Colors.white,
+              ),
             ),
-          ),
-          const SizedBox(height: 20),
-          Observer(
-            builder: (context) => Row(
+            const SizedBox(height: 20),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 if (!store.iniciado)
@@ -63,8 +65,8 @@ class Cronometro extends StatelessWidget {
                 ),
               ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
